b59a2fa93920516446c3e40531298dcb25d4aa24
awais3323 <awais381lead@gmail.com>
2024-03-28 02:07:14 +0500

    Graphql basics implemented with apollo server

--- Backend/src/server/index.ts
+++ Backend/src/server/index.ts
@@ -1,7 +1,12 @@
 import app from "./app";
-import chalk from "chalk"
-import dotenv from "dotenv"
+import chalk from "chalk";
+import dotenv from "dotenv";
+import typeDef from "../schema/shops/typeDefs";
+import testShops from "../schema/shops/resolver";
+import { ApolloServer } from 'apollo-server-express';
 import connectMongo from "../config/connectDatabase";
+import { applyMiddleware } from "graphql-middleware";
+import { makeExecutableSchema } from '@graphql-tools/schema'
 
 //Handling Uncaught Exception
 process.on("uncaught Exception", (err) => {
@@ -16,17 +21,33 @@ let port = process.env.PORT || 3000;
 // Database connection
 connectMongo();
 
-// Setting and server and assigning it a port
-const server = app.listen(port, () => {
-    console.log(`Server is up on ${chalk.green(".... ")} ${chalk.bgGreen(" "+ port+ " ")}`);
-});
+// Just for rough use for now
+let typeDefs: any[] = [typeDef]
+let resolvers: any[] = [{ Query: { testShops } }]
+const schema = makeExecutableSchema({
+    typeDefs,// typeDefs must be added here
+    resolvers, // Resolver must be added here
+})
 
+const schemaWithMiddleware = applyMiddleware(schema)
+let server = new ApolloServer({ schema: schemaWithMiddleware })
+
+// Apply middleware to the Express app
+
+// Top level await cannot be used 
+const startServer = async () =>  await server.start().then(()=> server.applyMiddleware({ app })) 
+startServer()
+
+// App listering to specific port
+app.listen(port, () => {
+    console.log(`Server is up on ${chalk.green(".... ")} ${chalk.bgGreen(" " + port + " ")}`);
+})
 
 //Unhandled promise rejection for ex: (if the server is crash because of the string that is added to connect databse)
 process.on("unhandledRejection", (err: { message: string }) => {
     console.log("Error", chalk.red(err.message));
     console.log(chalk.red("Shutting down the server"));
-    server.close(() => {
+    server.stop().then(() => {
         process.exit(1);
     });
-});
+})
